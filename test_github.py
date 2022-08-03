d087ca09b52cd3a1a0bd9bfd4384d6805a74bf48
Helpmerunaway <solovyovri@yandex.ru>
2022-08-03 21:10:41 +0300

    added parametrized tests

--- test_github.py
+++ test_github.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from selene import by
 from selene.support.conditions import be
@@ -7,12 +6,16 @@ from selene.support.shared import browser
 link = 'https://github.com'
 
 
-@pytest.mark.parametrize("window_size", ["400x817", "1600x900"])
-@pytest.mark.parametrize("platform", [1, 2], ids=["Desktop", "Mobile"])
-def test_github_selene(window_size, platform):
+@pytest.mark.parametrize("setup", [(1600, 900)], indirect=True)
+def test_github_desktop(setup):
+	browser.open(link)
+	browser.element('a[href="/login').click()
+	browser.element(by.text("Sign in to GitHub")).should(be.visible)
+
+
+@pytest.mark.parametrize("setup", [(414, 896)], indirect=True)
+def test_github_mobile(setup):
 	browser.open(link)
 	browser.element('[class="octicon octicon-three-bars"]').click()
 	browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]')
-	time.sleep(1)
 	browser.element(by.partial_text("Sign in")).should(be.visible)
-
